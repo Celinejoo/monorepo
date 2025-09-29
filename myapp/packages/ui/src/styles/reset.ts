044f7210750f3fc2ada3a6826dd7e56d7827b538
zoo <joohj0509@gmail.com>
2025-09-29 17:23:59 +0900

    reset.css, font-family 수정

--- myapp/packages/ui/src/styles/reset.ts
+++ myapp/packages/ui/src/styles/reset.ts
@@ -1,13 +1,17 @@
-/** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
 import { typography } from "@repo/tokens";
 
-// Pretendard 폰트 로드
 export const pretendardFont = css`
-  @import url("https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable-dynamic-subset.css");
+  @font-face {
+    font-family: "Pretendard";
+    src:
+      url("../font/subset-Pretendard-Regular.woff2") format(woff2),
+      url("../font/subset-Pretendard-Regular.woff") format(woff),
+      url("../font/subset-Pretendard-Regular.ttf") format(truetype);
+    font-display: fallback;
+  }
 `;
 
-// 글로벌 리셋
 export const globalReset = css`
   ${pretendardFont}
 
@@ -26,6 +30,131 @@ export const globalReset = css`
     font-family: ${typography.fontFamily.sans.join(", ")};
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+  }
+
+  html,
+  body,
+  div,
+  span,
+  applet,
+  object,
+  iframe,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p,
+  blockquote,
+  pre,
+  a,
+  abbr,
+  acronym,
+  address,
+  big,
+  cite,
+  code,
+  del,
+  dfn,
+  em,
+  img,
+  ins,
+  kbd,
+  q,
+  s,
+  samp,
+  small,
+  strike,
+  strong,
+  sub,
+  sup,
+  tt,
+  var,
+  b,
+  u,
+  i,
+  center,
+  dl,
+  dt,
+  dd,
+  ol,
+  ul,
+  li,
+  fieldset,
+  form,
+  label,
+  legend,
+  table,
+  caption,
+  tbody,
+  tfoot,
+  thead,
+  tr,
+  th,
+  td,
+  article,
+  aside,
+  canvas,
+  details,
+  embed,
+  figure,
+  figcaption,
+  footer,
+  header,
+  hgroup,
+  menu,
+  nav,
+  output,
+  ruby,
+  section,
+  summary,
+  time,
+  mark,
+  audio,
+  video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+  }
+
+  article,
+  aside,
+  details,
+  figcaption,
+  figure,
+  footer,
+  header,
+  hgroup,
+  menu,
+  nav,
+  section {
+    display: block;
+  }
+
+  ol,
+  ul {
+    list-style: none;
+  }
+
+  blockquote,
+  q {
+    quotes: none;
+  }
+
+  blockquote:before,
+  blockquote:after,
+  q:before,
+  q:after {
+    content: "";
+    content: none;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
   }
 
   button {
