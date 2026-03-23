8d792626b7d6ea9c15446b99791c52de7afd0e21
zoo <joohj0509@gmail.com>
2026-03-23 19:58:41 +0900

    스토리북 - 토큰 추가

--- myapp/packages/ui/.storybook/preview.tsx
+++ myapp/packages/ui/.storybook/preview.tsx
@@ -1,18 +1,26 @@
 import { Global } from "@emotion/react";
 import type { Preview } from "@storybook/react";
 import { globalReset } from "../src/styles/index";
+import { injectCssVariables, injectTypographyClasses } from "@repo/tokens";
 
 const preview: Preview = {
   parameters: {
     controls: { expanded: true },
   },
   decorators: [
-    (Story) => (
-      <>
-        <Global styles={globalReset} />
-        {Story()}
-      </>
-    ),
+    (Story) => {
+      if (typeof document !== "undefined") {
+        injectCssVariables();
+        injectTypographyClasses();
+      }
+
+      return (
+        <>
+          <Global styles={globalReset} />
+          {Story()}
+        </>
+      );
+    },
   ],
 };
 export default preview;
