72158a65795922647e9be3adcd29859a8a6269a6
zoo <joohj0509@gmail.com>
2026-03-25 20:57:16 +0900

    텍스트 필드 스토리북 수정, variant 추가

--- myapp/packages/ui/src/components/TextField/TextField.tsx
+++ myapp/packages/ui/src/components/TextField/TextField.tsx
@@ -3,26 +3,44 @@ import styled from "@emotion/styled";
 import { useId, useState } from "react";
 import { Flex } from "../FlexLayout";
 import { Paragraph } from "../Paragraph";
+import { css } from "@emotion/react";
 
-interface TextFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
+export interface TextFieldProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  variant?: "filled" | "outlined";
   label?: string;
   error?: boolean;
   helperText?: string;
 }
 
-const StyledInput = styled.input<{ error?: boolean }>`
-  font-size: 14px;
-  height: 32px;
-  border: none;
-  border-bottom: 1px solid var(--color-gray-400);
+const StyledInput = styled.input<TextFieldProps>`
+  font-size: var(--font-size-16);
+  height: 40px;
   width: 100%;
 
   &:focus {
     outline: none;
-    border-bottom: 1px solid;
-    border-color: ${({ error }) =>
-      error ? `var(--color-red-500)` : `var(--color-blue-500)`};
   }
+
+  ${({ variant = "outlined", error }) =>
+    variant === "outlined"
+      ? css`
+      border:none;
+      border-bottom: 1px solid var(--color-gray-400);
+
+      &:focus {
+        border-bottom: 1px solid;
+        border-bottom-color: ${error ? `var(--color-red-500)` : `var(--color-blue-500)`};
+    `
+      : css`
+          border: 1px solid var(--color-gray-400);
+          border-radius: 8px;
+          padding:8px;
+
+          &:focus {
+            border: 1px solid;
+            border-color: ${error ? `var(--color-red-500)` : `var(--color-blue-500)`};
+        `}
 `;
 
 const Label = ({
@@ -64,6 +82,7 @@ export const TextField = ({
   id,
   error,
   helperText,
+  variant,
   ...props
 }: TextFieldProps) => {
   const inputId = id ?? useId();
@@ -81,6 +100,7 @@ export const TextField = ({
         </Flex.Item>
         <Flex.Item>
           <StyledInput
+            variant={variant}
             id={inputId}
             error={error}
             aria-invalid={error}
