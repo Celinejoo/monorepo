c1a1c8ed5a5c2e2adb4042b9a97f3cd74be51c1e
zoo <joohj0509@gmail.com>
2026-03-29 22:51:05 +0900

    main.ts 수정

--- myapp/packages/ui/.storybook/main.ts
+++ myapp/packages/ui/.storybook/main.ts
@@ -1,3 +1,4 @@
+// packages/ui/.storybook/main.ts
 import type { StorybookConfig } from "@storybook/react-vite";
 import { mergeConfig } from "vite";
 
@@ -7,7 +8,16 @@ const config: StorybookConfig = {
   addons: ["@storybook/addon-essentials"],
   async viteFinal(config) {
     return mergeConfig(config, {
+      // 수정: "/" 대신 "./"를 사용하여 상대 경로로 빌드합니다.
       base: "./",
+      build: {
+        rollupOptions: {
+          output: {
+            // 언더바(_)로 시작하는 파일명을 방지합니다.
+            sanitizeFileName: (name: string) => name.replace(/^_+/, ""),
+          },
+        },
+      },
     });
   },
 };
